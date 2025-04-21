462d29b1edbaa7496ea138d0220dcb5529196967
rahultera <tera.r@northeastern.edu>
2025-04-21 12:10:22 -0400

    Update requireAuth.js

--- api/middleware/requireAuth.js
+++ api/middleware/requireAuth.js
@@ -5,6 +5,7 @@ export function requireAuth(req, res, next) {
     console.error("No token provided");
     return res.status(401).json({ error: "You are not logged in" });
   }
+ res.cookie("token", token, { httpOnly: true, maxAge: 15 * 60 * 1000, sameSite: 'none', secure: true });
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = payload.userId;
