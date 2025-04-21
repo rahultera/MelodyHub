be829b0370754cc0c30324facbfd27c949ed9eab
rahultera <tera.r@northeastern.edu>
2025-04-21 06:34:47 -0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -24,10 +24,11 @@ import { requireAuth } from "./middleware/requireAuth.js";
 
 const app = express();
 
-app.use(cors({ 
-  origin: "http://localhost:3000", 
-  credentials: true 
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://melody-hub-wheat-two.vercel.app'],
+  credentials: true
 }));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
